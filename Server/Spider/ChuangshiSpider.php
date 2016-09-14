7524bc5c7d2b9a5eb54e34f2f2c1ef04b88b49de
smartdj <smart.dj@163.com>
2016-09-15 02:29:24 +0800

    添加创世网的爬虫，该写获取小说名称的代码了。

--- Server/Spider/ChuangshiSpider.php
+++ Server/Spider/ChuangshiSpider.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: dj
- * Date: 2016/9/3
- * Time: 1:16
+ * Date: 2016/9/7
+ * Time: 20:39
  */
 
 namespace Spider;
@@ -28,9 +28,9 @@ class QidianBookModel
     public $writtenWords;
 }
 
-class QidianSpider
+class ChuangshiSpider
 {
-    private static $baseURL = "http://a.qidian.com/?size=-1&sign=-1&tag=-1&chanId=-1&subCateId=-1&orderId=&page=%d&month=-1&style=1&action=-1&vip=-1";
+    private static $baseURL = "http://chuangshi.qq.com/bk/p/%d.html";
 
     static function getFullURLWithPageNumber($pageNumber){
         return sprintf(self::$baseURL,$pageNumber);
@@ -40,16 +40,19 @@ class QidianSpider
         $pageURL = self::getFullURLWithPageNumber($pageNumber);
 
         $result = WebRequest::get($pageURL, WebRequest::genHeaders($pageURL));
-        //showResult($result);
+
         $html_dom = str_get_html($result);
         return $html_dom;
     }
 
     public function getMaxPage($html_dom){
-        $pageMaxDiv = $html_dom->find('div[data-pageMax]',0);
 
-        if($pageMaxDiv){
-            return $pageMaxDiv->attr['data-pagemax'];
+        $pageMaxEM = $html_dom->find('div.Pagination em',1);
+        //print_r($pageMaxEM);
+        if($pageMaxEM){
+            $text = $pageMaxEM->innertext;
+
+            return mb_substr($text, 2, mb_strlen($text, "utf-8") - 3, "utf-8");
         }
 
         return 0;
@@ -58,62 +61,62 @@ class QidianSpider
     public function getBooksBaseInfo($html_dom){
         $booksInfo = array();
 
-        $books = $html_dom->find("li[data-rankid]");
-        foreach ($books as $bookli){
+        $books = $html_dom->find("tr[class!=underline]");
+        foreach ($books as $booktr){
 
             $bookDataModel = new QidianBookModel();
 
             //获取bookid
-            $bookIdElem = $bookli->find("a[data-bid]", 0);
+            $bookIdElem = $booktr->find("a[data-bid]", 0);
             if($bookIdElem){
                 $bookDataModel->Id = $bookIdElem->attr["data-bid"];
                 $bookDataModel->URL = $bookIdElem->href;
             }
 
             //获取小说封面
-            $bookImageElem = $bookli->find("div.book-img-box a img", 0);
+            $bookImageElem = $booktr->find("div.book-img-box a img", 0);
             if($bookImageElem){
                 $bookDataModel->imgURL = $bookImageElem->attr["src"];
             }
 
             //获取书名
-            $bookNameElem = $bookli->find("div h4 a",0);
+            $bookNameElem = $booktr->find("div h4 a",0);
             if($bookNameElem){
                 $bookDataModel->name = $bookNameElem->innertext;
             }
 
             //获取作者
-            $bookAuthorElem = $bookli->find("div p a[class=name]", 0);
+            $bookAuthorElem = $booktr->find("div p a[class=name]", 0);
             if($bookAuthorElem){
                 $bookDataModel->authorName = $bookAuthorElem->innertext;
                 $bookDataModel->authorURL = $bookAuthorElem->href;
             }
 
             //获取小说主分类
-            $mainCategoryElem = $bookli->find("div.book-mid-info p.author a[data-aid=qd_B60]",0);
+            $mainCategoryElem = $booktr->find("div.book-mid-info p.author a[data-aid=qd_B60]",0);
             if($mainCategoryElem){
                 $bookDataModel->mainCategory = $mainCategoryElem->innertext;
             }
             //获取小说子分类
-            $subCategoryElem = $bookli->find("div.book-mid-info p.author a[data-aid=qd_B61]",0);
+            $subCategoryElem = $booktr->find("div.book-mid-info p.author a[data-aid=qd_B61]",0);
             if($mainCategoryElem){
                 $bookDataModel->subCategory = $subCategoryElem->innertext;
             }
 
             //获取小说写作状态
-            $statusElem = $bookli->find("div.book-mid-info p.author span",0);
+            $statusElem = $booktr->find("div.book-mid-info p.author span",0);
             if($statusElem){
                 $bookDataModel->status = $statusElem->innertext;
             }
 
             //获取小说短简介
-            $shortDescriptionElem = $bookli->find("p.intro",0);
+            $shortDescriptionElem = $booktr->find("p.intro",0);
             if($shortDescriptionElem){
                 $bookDataModel->shortDescription = trim($shortDescriptionElem->innertext);
             }
 
             //获取小说字数
-            $writtenWrodsElem = $bookli->find("p.update span",0);
+            $writtenWrodsElem = $booktr->find("p.update span",0);
             if($writtenWrodsElem){
                 $bookDataModel->writtenWords = $writtenWrodsElem->innertext;
             }
@@ -139,28 +142,26 @@ class QidianSpider
 
         return $booksInfo;
     }
-
 }
 
 //取消脚本最大时间限制
-set_time_limit(0);
+//set_time_limit(0);
 
-$qidianSpider = new QidianSpider();
+$chuangshiSpider = new ChuangshiSpider();
 $startPageNumber = 1;
 $maxPage = $startPageNumber+1;//初始先设置为2,为了让循环能顺利运行下去
 for ($page=$startPageNumber; $page<$maxPage; $page++){
 
-    $html_dom = $qidianSpider->getContentWithPageNumber($page);
+    $html_dom = $chuangshiSpider->getContentWithPageNumber($page);
 
     if($page==$startPageNumber){
-        $maxPage = $qidianSpider->getMaxPage($html_dom);
+        $maxPage = $chuangshiSpider->getMaxPage($html_dom);
         echo $maxPage;
         echo "<br/>";
     }
 
-    $booksInfo = $qidianSpider->getBooksBaseInfo($html_dom);
-
+    $booksInfo = $chuangshiSpider->getBooksBaseInfo($html_dom);
+    break;
     //sleep(1);
     //print_r($booksInfo);
 }
-
