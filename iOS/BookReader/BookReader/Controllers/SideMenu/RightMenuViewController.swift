05806661ea4973fb433ab4f23fbd94e5347d745b
DJ <Arthur@miduos-Mac-mini.local>
2016-11-03 16:25:42 +0800

    iOS客户端，添加主界面右侧菜单

--- iOS/BookReader/BookReader/Controllers/SideMenu/RightMenuViewController.swift
+++ iOS/BookReader/BookReader/Controllers/SideMenu/RightMenuViewController.swift
@@ -8,10 +8,70 @@
 
 import UIKit
 
-class RightMenuViewController: UIViewController {
+class RightMenuViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+    
+    lazy var tableView:UITableView? = {
+        //初始化
+        var tableView = UITableView();
+        //注册Cell
+        tableView.registerClass(RightSideMenuTableViewCell.self, forCellReuseIdentifier: kDefaultCellIdentifier)
+        
+        //设置数据源和代理
+        tableView.delegate = self;
+        tableView.dataSource = self;
+        tableView.backgroundColor = UIColor.colorFromRBG(0x1c1c1c);
+        tableView.separatorColor = UIColor.colorFromRBG(0x1d1d1d);//设置分割线颜色
+        tableView.separatorInset = UIEdgeInsetsMake(0, 0, 0, 0);
+        tableView.tableFooterView = UIView(frame: CGRectZero);
+        return tableView;
+    }()
+    
+    //设置单元格数据，使用懒加载
+    lazy var dataSource: [SideMenuTableViewCellModel] = {
+        return [SideMenuTableViewCellModel(title: "搜索", icon: UIImage.init(named: "侧边栏_右侧边栏_搜索"), isSelected:false)
+            ,SideMenuTableViewCellModel(title: "书城", icon: UIImage.init(named: "侧边栏_右侧边栏_书城"), isSelected:false)
+            ,SideMenuTableViewCellModel(title: "包月专区", icon: UIImage.init(named: "侧边栏_右侧边栏_包月专区"), isSelected:false)
+            ,SideMenuTableViewCellModel(title: "排行榜", icon: UIImage.init(named: "侧边栏_右侧边栏_排行榜"), isSelected:false)
+            ,SideMenuTableViewCellModel(title: "主题数单", icon: UIImage.init(named: "侧边栏_右侧边栏_主题书单"), isSelected:false)
+            ,SideMenuTableViewCellModel(title: "分类", icon: UIImage.init(named: "侧边栏_右侧边栏_分类"), isSelected:false)
+            ,SideMenuTableViewCellModel(title: "听书专区", icon: UIImage.init(named: "侧边栏_右侧边栏_听书专区"), isSelected:false)
+            ,SideMenuTableViewCellModel(title: "随机看书", icon: UIImage.init(named: "侧边栏_右侧边栏_随机看书"), isSelected:false)];
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.redColor();
+        view.backgroundColor = UIColor.whiteColor();
+        
+        //添加tableview
+        view.addSubview(tableView!);
+        
+        tableView!.snp_makeConstraints { [unowned self] (make) -> Void in
+            make.left.right.bottom.equalTo(self.view);
+            make.top.equalTo(kStatusBarHeight);
+        };
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return dataSource.count;
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 53.0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(kDefaultCellIdentifier, forIndexPath: indexPath) as! RightSideMenuTableViewCell
+        
+        // 配置cell
+        cell.dataModel = self.dataSource[indexPath.row];
+        // 返回cell
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
 }
--- iOS/BookReader/BookReader/Controllers/SideMenu/View/RightSideMenuTableViewCell.swift
+++ iOS/BookReader/BookReader/Controllers/SideMenu/View/RightSideMenuTableViewCell.swift
@@ -9,16 +9,66 @@
 import UIKit
 
 class RightSideMenuTableViewCell: UITableViewCell {
-
+    private var _dataModel:SideMenuTableViewCellModel?
+    var dataModel:SideMenuTableViewCellModel?{
+        set{
+            _dataModel = newValue;
+            self.setSelected((_dataModel?.isSelected)!, animated: false);
+            self.iconImageView?.image = newValue?.icon;
+            self.textLabel?.text = _dataModel?.title
+        }
+        get{
+            return _dataModel;
+        }
+    }
+    
+    lazy var iconImageView:UIImageView? = {
+        var iconImageView = UIImageView();
+        self.contentView.addSubview(iconImageView);
+        iconImageView.snp_makeConstraints(closure: {[unowned self] (make) in
+            make.left.equalTo(14);
+            make.centerY.equalTo(self);
+            make.width.height.equalTo(30);
+            })
+        return iconImageView;
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.textLabel?.frame.origin = CGPointMake(58, (self.textLabel?.frame.origin.y)!)
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
+    override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
+    
+    override func setHighlighted(highlighted: Bool, animated: Bool) {
+        super.setHighlighted(highlighted, animated: animated)
+    }
 
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier);
+        
+        self.backgroundColor = UIColor.colorFromRBG(0x282828);
+        self.selectionStyle = .Gray;
+        self.textLabel?.textColor = UIColor.whiteColor()
+        
+        //修改选中时的高亮背景色
+        let v:UIView = UIView();
+        v.backgroundColor = UIColor.blackColor();
+        self.selectedBackgroundView = v;
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder);
+    }
+    
+    
 }
